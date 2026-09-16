4d35dd25649d742dc91b041902cf5ad8090e1d8f
mehfoozkhan03 <mehfooznettech786@gmail.com>
2026-09-16 10:22:21 +0530

    code of login page and form components

---
+++ Frontend/src/Components/Form.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import "../Style/Form.css";
+/* 
+{
+name:"nfg",
+email:"nfgf",
+password:"nfgfr"
+}
+*/
+
+export const Form = ({ props, button }) => {
+  const [form, setForm] = React.useState(() => {
+    return props.reduce((acc, curr) => {
+      acc[curr.name] = "";
+      return acc;
+    }, {});
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name);
+    setForm((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  console.log(form);
+  return (
+    <form className="form">
+      {props?.map((el) => (
+        <div key={el.id}>
+          <label htmlFor={el.name}>{el.name}</label>
+          <input
+            type={el.type}
+            name={el.name}
+            placeholder={el.placeholder}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+      ))}
+      <button>{button}</button>
+    </form>
+  );
+};
--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Form } from '../Components/Form'
 
 export const Login = () => {
+  const data=[
+    {
+      id:1,
+      name:"name",
+      type:"text",
+      placeholder:"enter you'r name..."
+    },
+    {
+      id:2,
+      name:"email",
+      type:"email",
+      placeholder:"enter you'r email..."
+    },
+    {
+      id:3,
+      name:"password",
+      type:"password",
+      placeholder:"enter you'r password..."
+    }
+  ]
   return (
-    <div>Login</div>
+    <Form props={data} button="login"/>
   )
 }
